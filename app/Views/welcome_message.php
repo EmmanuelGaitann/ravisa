5c3167b5f17c1d92ca3683f81d6799e0573c6a20
EmmanuelGaitann <118638965+EmmanuelGaitann@users.noreply.github.com>
2025-11-28 21:11:15 +0100

    1111 welcome

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -238,7 +238,7 @@
 <section>
 
     <h1>About this page</h1>
-CECI EST UNTEST, j'encaisse une antithèse ddpdkdkmlkqsf,
+CECI EST UNTEST, TTEST TEST
     <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
     <p>If you would like to edit this page you will find it located at:</p>
